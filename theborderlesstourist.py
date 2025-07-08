b4f64af6d1159d1627f85483c848252c41c49937
Brandon Mawson <bran.maws@gmail.com>
2025-07-07 19:33:41 -0700

    Added function to generate message for traveler and present attractions they might be interested in.

--- theborderlesstourist.py
+++ theborderlesstourist.py
@@ -49,5 +49,15 @@ def find_attractions(destination, interests):
             if interest in attraction_tags:
               attractions_with_interest.append(possible_attraction[0])
     return attractions_with_interest
-la_arts = find_attractions("Los Angeles, USA", ['art'])
-print(la_arts)
+
+# Function to get the attractions for a traveler's destination
+def get_attractions_for_traveler(traveler):
+    traveler_destination = traveler[1]
+    traveler_interests = traveler[2]
+    traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+    interests_string = "Hi " + traveler[0] + ", we think you'll like these places around " + traveler_destination + ": "
+    for attraction in traveler_attractions:
+        interests_string += attraction
+    return interests_string
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
